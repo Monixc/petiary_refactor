b6ea741cc98317cf5376be9c947a0e52edf57aa3
Monixc <chris13404@gmail.com>
2024-12-01 23:57:16 +0900

    fix: fix build error

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  /* config options here */
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
-        hostname: process.env.AWS_S3_HOSTNAME || "",
-        port: "",
-        pathname: "/uploads/**",
-      },
-      {
-        protocol: "https",
-        hostname: process.env.DALLE_API_HOSTNAME || "",
+        hostname: "*",
         port: "",
         pathname: "/**",
       },
