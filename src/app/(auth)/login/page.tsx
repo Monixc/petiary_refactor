e6966f9828765dd0710d7bcaeea330d30c3863ed
Monixc <chris13404@gmail.com>
2024-12-01 22:54:20 +0900

    feat: edit router path

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -22,7 +22,7 @@ export default function LoginPage() {
         <Button
           variant="outline"
           className="w-full max-w-xs flex items-center justify-center gap-2 mt-8"
-          onClick={() => router.push("/home")}>
+          onClick={() => router.push("/diary")}>
           <img src="/google-logo.svg" alt="Google" className="w-5 h-5" />
           Google로 계속하기
         </Button>
